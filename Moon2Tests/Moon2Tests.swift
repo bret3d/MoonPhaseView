c0cbbee7ae45902fc76b841dcbd2a60a29ebe960
Bret Dahlgren <bret3d@yahoo.com>
2015-03-30 14:26:15 -0400

    Initial Commit

---
+++ Moon2Tests/Moon2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Moon2Tests.swift
+//  Moon2Tests
+//
+//  Created by Bret Dahlgren on 3/30/15.
+//  Copyright (c) 2015 Bret Dahlgren. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Moon2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
